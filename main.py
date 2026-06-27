f30d38bbbd0f3d97214c01fcbc961598a29f7f96
prajaswa2001-ui <prajaswa2001@gmail.com>
2026-06-27 17:22:29 +0700

    wall collision

--- main.py
+++ main.py
@@ -18,10 +18,26 @@ class GameSprite(sprite.Sprite):
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
 
-rackek1 = GameSprite("racket.png", 30,200,4,50,150)
-rackek2 = GameSprite("racket.png", 520,200,4,50,150)
+class Player(GameSprite):
+    def update_1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+    def update_2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+rackek1 = Player("racket.png", 30,200,4,50,150)
+rackek2 = Player("racket.png", 520,200,4,50,150)
 ball = GameSprite("tennis.png",200,200,4,50,50)
 game = True
+fps = 60
+clock = time.Clock()
 
 while game:
     for e in event.get():
@@ -33,4 +49,9 @@ while game:
     rackek2.reset()
     ball.reset()
 
+    rackek1.update_1()
+    rackek2.update_2()
+
+
     display.update()
+    clock.tick(fps)
