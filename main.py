21a2d938560ed8208a632baa2a2fe642368a1a97
prajaswa2001-ui <prajaswa2001@gmail.com>
2026-06-27 17:05:15 +0700

    sprites

--- main.py
+++ main.py
@@ -3,13 +3,34 @@ from pygame import *
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
-window.fill((30, 255, 0))
+background = (30, 255, 50)
+window.fill(background)
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) 
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+rackek1 = GameSprite("racket.png", 30,200,4,50,150)
+rackek2 = GameSprite("racket.png", 520,200,4,50,150)
+ball = GameSprite("tennis.png",200,200,4,50,50)
 game = True
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    window.fill(background)
+    rackek1.reset()
+    rackek2.reset()
+    ball.reset()
 
     display.update()
